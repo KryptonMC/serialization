d3e325ef8ed0b07393204832fd438a5c7c30d65a
Callum Seabrook <callum.seabrook@prevarinite.com>
2022-08-10 22:42:55 +0100

    Fixed licensing

--- nbt/src/main/java/org/kryptonmc/serialization/nbt/NbtRecordBuilder.java
+++ nbt/src/main/java/org/kryptonmc/serialization/nbt/NbtRecordBuilder.java
@@ -1,3 +1,11 @@
+/*
+ * This file is part of Krypton Serialization, licensed under the MIT license.
+ *
+ * Copyright (C) 2022 KryptonMC and contributors
+ *
+ * This project is licensed under the terms of the MIT license.
+ * For more details, please reference the LICENSE file in the top-level directory.
+ */
 package org.kryptonmc.serialization.nbt;
 
 import org.jetbrains.annotations.NotNull;
