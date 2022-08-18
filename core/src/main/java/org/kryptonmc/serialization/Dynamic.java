bb6886ae944bc56562001462609fddeab1fc9773
Callum Seabrook <callum.seabrook@prevarinite.com>
2022-08-18 16:25:34 +0100

    Fixed licensing

--- core/src/main/java/org/kryptonmc/serialization/Dynamic.java
+++ core/src/main/java/org/kryptonmc/serialization/Dynamic.java
@@ -1,3 +1,16 @@
+/*
+ * This file is part of Krypton Serialization, and originates from the Data
+ * Fixer Upper, licensed under the MIT license.
+ *
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * Copyright (C) 2022 KryptonMC and contributors
+ *
+ * This project is licensed under the terms of the MIT license.
+ * For more details, please reference the LICENSE file in the top-level directory.
+ *
+ * For the original file that this file is derived from, see here:
+ * https://github.com/Mojang/DataFixerUpper/blob/c100ef03c2ab321e5de4f25ffbe277e924aa7ca5/src/main/java/com/mojang/serialization/Dynamic.java
+ */
 package org.kryptonmc.serialization;
 
 import java.nio.ByteBuffer;
